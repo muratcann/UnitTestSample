070bfeec5648e2f94506c437ede18acb4e17fea8
MURAT CAN <MURAT.CAN@lcwaikiki.com>
2022-08-31 16:37:04 +0300

    Add project files.

---
+++ UnitTestSample.Web/Models/UnitTestDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace UnitTestSample.Web.Models
+{
+    public partial class UnitTestDbContext : DbContext
+    {
+        public UnitTestDbContext()
+        {
+        }
+
+        public UnitTestDbContext(DbContextOptions<UnitTestDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
